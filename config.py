69e07ff55aec02254390b4f22a2788f5a659453b
Regan Sarwas <regan_sarwas@nps.gov>
2017-07-27 10:48:21 -0800

    Define the server setting

--- config.py
+++ config.py
@@ -11,3 +11,11 @@ Path can use a unix style forward slash (/) as a directory separator
 otherwise it must be a raw string (r"c:\tmp") to avoid special meaning of back slash (\)
 """
 root_directory = 'c:/tmp/pubtest'
+
+"""
+The default server type/connection file.  Must be a quoted string or None
+A quoted string should be either 'MY_HOSTED_SERVICES' or a valid file path.
+if this setting is missing, None or any other non-valid value, it will default
+to 'MY_HOSTED_SERVICES'
+"""
+service = 'c:/tmp/pubtest/server.asg'
