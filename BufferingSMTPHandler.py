4b12cafa411321092d812187e1e7425ddef2c305
Regan Sarwas <Regan_Sarwas@nps.gov>
2021-01-28 11:29:23 -0900

    Cleanup buffering mail handler

--- BufferingSMTPHandler.py
+++ BufferingSMTPHandler.py
@@ -1,34 +1,41 @@
-# From https://gist.github.com/anonymous/1379446
-#
-# Copyright 2001-2002 by Vinay Sajip. All Rights Reserved.
-#
-# Permission to use, copy, modify, and distribute this software and its
-# documentation for any purpose and without fee is hereby granted,
-# provided that the above copyright notice appear in all copies and that
-# both that copyright notice and this permission notice appear in
-# supporting documentation, and that the name of Vinay Sajip
-# not be used in advertising or publicity pertaining to distribution
-# of the software without specific, written prior permission.
-# VINAY SAJIP DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE, INCLUDING
-# ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL
-# VINAY SAJIP BE LIABLE FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR
-# ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER
-# IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT
-# OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
-#
-# This file is part of the Python logging distribution. See
-# http://www.red-dove.com/python_logging.html
-#
-"""Test harness for the logging module. Tests BufferingSMTPHandler, an alternative implementation
-of SMTPHandler.
 # -*- coding: utf-8 -*-
-Copyright (C) 2001-2002 Vinay Sajip. All Rights Reserved.
 """
+An alternative implementation of SMTPHandler that aggregates messages.
+
+From https://gist.github.com/anonymous/1379446
+
+Copyright (C) 2001-2002 Vinay Sajip. All Rights Reserved.
+
+Permission to use, copy, modify, and distribute this software and its
+documentation for any purpose and without fee is hereby granted,
+provided that the above copyright notice appear in all copies and that
+both that copyright notice and this permission notice appear in
+supporting documentation, and that the name of Vinay Sajip
+not be used in advertising or publicity pertaining to distribution
+of the software without specific, written prior permission.
+VINAY SAJIP DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE, INCLUDING
+ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL
+VINAY SAJIP BE LIABLE FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR
+ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER
+IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT
+OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+This file is part of the Python logging distribution. See
+http://www.red-dove.com/python_logging.html
+"""
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
 import logging
 import logging.handlers
+import smtplib
+
+# pylint: disable=too-many-arguments,bare-except
 
 
 class BufferingSMTPHandler(logging.handlers.BufferingHandler):
+    """A logging handler that buffers (aggregates) SMTP messages."""
+
     def __init__(self, mailhost, fromaddr, toaddrs, subject, capacity=100):
         logging.handlers.BufferingHandler.__init__(self, capacity)
         self.mailhost = mailhost
@@ -39,20 +46,20 @@ class BufferingSMTPHandler(logging.handlers.BufferingHandler):
         self.setFormatter(logging.Formatter("%(asctime)s %(levelname)-5s %(message)s"))
 
     def flush(self):
+        """Flush the aggregated messages to the SMTP server."""
         if len(self.buffer) > 0:
-            # noinspection PyBroadException
             try:
-                import smtplib
                 port = self.mailport
                 if not port:
                     port = smtplib.SMTP_PORT
                 smtp = smtplib.SMTP(self.mailhost, port)
-                msg = "From: %s\r\nTo: %s\r\nSubject: %s\r\n\r\n" % (self.fromaddr,
-                                                                     ','.join(self.toaddrs),
-                                                                     self.subject)
+                msg = "From: %s\r\nTo: %s\r\nSubject: %s\r\n\r\n" % (
+                    self.fromaddr,
+                    self.toaddrs.join(","),
+                    self.subject,
+                )
                 for record in self.buffer:
-                    s = self.format(record)
-                    msg = msg + s + "\r\n"
+                    msg += self.format(record) + "\r\n"
                 smtp.sendmail(self.fromaddr, self.toaddrs, msg)
                 smtp.quit()
             except:
